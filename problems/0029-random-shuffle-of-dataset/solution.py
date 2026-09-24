7eb6b1800b2c18783f1c7d6c175a460b6aa55877
yassinzayed <302177818+yassinzayed@users.noreply.github.com>
2026-09-24 12:31:44 +0000

    Update: Random Shuffle of Dataset

--- problems/0029-random-shuffle-of-dataset/solution.py
+++ problems/0029-random-shuffle-of-dataset/solution.py
@@ -2,10 +2,7 @@ import numpy as np
 
 def shuffle_data(X, y, seed=None):
 	rng = np.random.seed(seed)
-	indices = np.random.permutation(range(0, len(X))).tolist()
-	X_new = []
-	y_new = []
-	for i in indices:
-		X_new.append(X[i].tolist())
-		y_new.append(y[i])
+	indices = np.random.permutation(len(X))
+	X_new = X[indices]
+	y_new = y[indices]
 	return (X_new, y_new)
