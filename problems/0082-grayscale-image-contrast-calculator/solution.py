8908bab64e46234a157d5214a780eaa2c9343b69
yassinzayed <302177818+yassinzayed@users.noreply.github.com>
2026-09-26 14:24:05 +0000

    Update: Grayscale Image Contrast Calculator

--- problems/0082-grayscale-image-contrast-calculator/solution.py
+++ problems/0082-grayscale-image-contrast-calculator/solution.py
@@ -6,5 +6,6 @@ def calculate_contrast(img) -> int:
 	Args:
 		img (numpy.ndarray): 2D array representing a grayscale image with pixel values between 0 and 255.
 	"""
+	# array.reshape(-1) and array.flatten() and array.ravel() flatten the given array
 	contrast = max(img.reshape(-1))-min(img.reshape(-1))
 	return contrast
